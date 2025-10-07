ee70c8208ea3597dd3b6c0ff3b8cd51902524b72
Theodoros Stefanidis <stefanidhes@gmail.com>
2025-10-08 01:23:00 +0300

    Update tools.js

--- js/tools.js
+++ js/tools.js
@@ -563,7 +563,7 @@ openOptionsDatatable = async function (wallet) {
             if (i === dragoLength - 1) {
                 rentDragonsNumber = await countRentalDragons(wallet);
                 avSuccessRate = parseFloat(parseFloat(sumSuccessRate) / parseInt(rentDragonsNumber)).toFixed(2);
-                totalData = [{label: "Dragons Number", val: dragoLength}, {label: "Total DSA", val: totalDSAEarned}, {label: "Total Owner DSA", val: totalTotalOwnerProfit}, {label: "Total Rental Times", val: totalTotalRentalTimes}, {label: "Total Gathering Times", val: totalTotalMiningTimes}, {label: "Total UnclaimedProfit Profit", val: totalUnclaimedProfit}, {label: "Total Cost", val: Math.round(rentTotalCost)}, {label: "Total Used", val: Math.round(rentTotalUsed)}];
+                totalData = [{label: "Dragons Number", val: dragoLength}, {label: "Total DSA", val: totalDSAEarned}, {label: "Total Owner DSA", val: totalTotalOwnerProfit}, {label: "Total Rental Times", val: totalTotalRentalTimes}, {label: "Total Gathering Times", val: totalTotalMiningTimes}, {label: "Total Unclaimed Profit", val: totalUnclaimedProfit}, {label: "Total Cost", val: Math.round(rentTotalCost)}, {label: "Total Used", val: Math.round(rentTotalUsed)}];
                 rentalData = [{label: "Dragons Number", val: rentDragonsNumber}, {label: "Total DSA", val: rentTotalDsaEarned}, {label: "Total Owner DSA", val: rentOwnerTotalProfit}, {label: "Total Daily DSA", val: rentTotalDailyMined}, {label: "Total Daily Owner DSA", val: Math.round(rentOwnerDailyMined)}, {label: "Total Gathering Times", val: rentTotalMiningTimes}, {label: "Average Success Rate", val: avSuccessRate + ' %'}];
             }
         }
